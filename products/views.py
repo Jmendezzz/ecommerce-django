3dc04578268cdb6c7c51f753a42a918568d2b807
Juan Gerardo <jmendez1025@cue.edu.co>
2025-09-09 23:34:25 -0500

    feat: templates and styles

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+# Views define the html file and gives the data.
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, "products/category_list.html", {"categories": categories})
